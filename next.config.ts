8262963356d41d5430efe3b0a7f64817c78dfaee
Johann Roth <johann.roth@esveo.com>
2024-11-29 20:04:21 +0100

    removes build output and updates github action workflow

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  // required option to deploy to github pages
-  distDir: "docs",
 };
 
 export default nextConfig;
