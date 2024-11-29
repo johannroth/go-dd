8d312edf0a38055a6d0c5369cb96332a4327c415
Johann Roth <johann.roth@esveo.com>
2024-11-29 19:31:32 +0100

    updates build task and runs first build with demo page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  // required option to deploy to github pages
+  distDir: "docs",
 };
 
 export default nextConfig;
